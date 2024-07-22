ff6a0faa084e10733fec6d21cfa1ed4e51c14340
kemboi22 <kemboielvis22@gmail.com>
2024-07-22 10:17:01 +0300

    feat(livestock): added livestocks

---
+++ app/Models/Livestock.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Livestock extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'breed',
+        'birth_date',
+        'weight',
+        'health_status',
+    ];
+}
