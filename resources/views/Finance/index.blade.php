eef049aeecec4ac1ef3520cd256799a58d795103
kemboi22 <kemboielvis22@gmail.com>
2024-07-22 19:05:20 +0300

    feat(finance): added creation of finances

--- resources/views/Finance/index.blade.php
+++ resources/views/Finance/index.blade.php
@@ -6,6 +6,10 @@
             <h3 class="block-title">Finances</h3>
         </div>
         <div class="block-content block-content-full">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createFinance">
+                Create Finance
+            </button>
             <!-- DataTables init on table by adding .js-dataTable-buttons class, functionality is initialized in js/pages/tables_datatables.js -->
             <table class="table table-bordered table-striped table-vcenter js-dataTable-buttons">
                 <thead>
@@ -41,6 +45,45 @@
                 @endforeach
                 </tbody>
             </table>
+        </div>
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="createFinance" tabindex="-1" aria-labelledby="createFinanceLabel"
+             aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="createFinanceLabel">Finance</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="{{ route('finances.store') }}" method="POST">
+                    <div class="modal-body">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="item" class="form-label">Item</label>
+                                <input type="text" class="form-control" id="item" name="item">
+                            </div>
+                            <div class="mb-3">
+                                <label for="cost" class="form-label">Cost</label>
+                                <input type="text" class="form-control" id="cost" name="cost">
+                            </div>
+                            <div class="mb-3">
+                                <label for="date" class="form-label">Date</label>
+                                <input type="date" class="form-control" id="date" name="date">
+                            </div>
+                            <div class="mb-3">
+                                <label for="category" class="form-label">Category</label>
+                                <input type="text" class="form-control" id="category" name="category">
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                </form>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
