bb7768743ab22cc73cb63ce0c356c7e0a1983212
kemboi22 <kemboielvis22@gmail.com>
2024-07-22 13:31:41 +0300

    feat(procurements): added crud for procurements

--- app/Http/Controllers/ProcurementController.php
+++ app/Http/Controllers/ProcurementController.php
@@ -12,15 +12,9 @@ class ProcurementController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return view('Procurement.index', [
+            "procurements" => Procurement::all()
+        ]);
     }
 
     /**
@@ -28,7 +22,27 @@ class ProcurementController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $procurement = Procurement::query()->create([
+            'item' => $request->item,
+            'quantity' => $request->quantity,
+            'cost' => $request->cost,
+            'type' => $request->type,
+            'date' => $request->date,
+            'payment_mode' => $request->payment_mode,
+            'transaction_id' => $request->transaction_id
+        ]);
+        if ($procurement)
+        {
+            return to_route('procurements.index')->with([
+                "message" => "Procurements created Successfully",
+                "success" => true
+            ]);
+
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" =>false
+        ]);
     }
 
     /**
@@ -50,16 +64,61 @@ class ProcurementController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Procurement $procurement)
+    public function update(Request $request, string $id)
     {
-        //
+        $procurement = Procurement::query()->findOrFail($id);
+        if ($procurement)
+        {
+            $updated = $procurement->update([
+                'item' => $request->item,
+                'quantity' => $request->quantity,
+                'cost' => $request->cost,
+                'type' => $request->type,
+                'date' => $request->date,
+                'payment_mode' => $request->payment_mode,
+                'transaction_id' => $request->transaction_id
+            ]);
+            if ($updated)
+            {
+                return to_route('procurements.index')->with([
+                    "message" => "Procurements updated successfully",
+                    "success" => true
+                ]);
+            }
+            return back()->with([
+               "message" => "An error occurred",
+               "success" => false
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Procurement $procurement)
+    public function destroy(string $id)
     {
-        //
+        $procurement = Procurement::query()->findOrFail($id);
+        if ($procurement)
+        {
+            if ($procurement->delete())
+            {
+                return to_route('procurements.index')->with([
+                   "message" => "Deleted successfully",
+                   "success" => true
+                ]);
+            }
+            return back()->with([
+                "message" => "An error occurred",
+                "success" => false
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
     }
 }
