659c586b990d48b81371e1fd26dd047918ebc0a6
kemboi22 <kemboielvis22@gmail.com>
2024-07-22 13:09:52 +0300

    feat(finances): added crud for finances

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -12,15 +12,9 @@ class FinanceController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return view('Finance.index', [
+            "finances" => Finance::all()
+        ]);
     }
 
     /**
@@ -28,7 +22,23 @@ class FinanceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $finance = Finance::query()->create([
+            'item' => $request->item,
+            'cost' => $request->cost,
+            'date' => $request->date,
+            'category' => $request->category,
+        ]);
+        if ($finance)
+        {
+            return to_route('finances.index')->with([
+                "message" => "Finances created successfully",
+                "success" => true
+            ]);
+        }
+        return to_route('finances.index')->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
     }
 
     /**
@@ -40,26 +50,60 @@ class FinanceController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Finance $finance)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Finance $finance)
+    public function update(Request $request, string $id)
     {
-        //
+        $finance = Finance::query()->findOrFail($id);
+        if ($finance)
+        {
+            $updated = $finance->update([
+                'item' => $request->item,
+                'cost' => $request->cost,
+                'date' => $request->date,
+                'category' => $request->category,
+            ]);
+            if ($updated)
+            {
+                return to_route('finances.index')->with([
+                   "message" => "Updated Successfully",
+                   "success" => true
+                ]);
+            }
+            return back() ->with([
+                "message" => "An error occurred",
+                "success" => true
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Finance $finance)
+    public function destroy(string $id)
     {
-        //
+        $finance = Finance::query()->findOrFail($id);
+        if ($finance)
+        {
+            if ($finance->delete())
+            {
+                return to_route('finances.index')->with([
+                    "message" => "Finance deleted successfully",
+                    "success" => true
+                ]);
+            }
+            return back()->with([
+                "message" => "An error occurred",
+                "success" => false
+            ]);
+        }
+        return back()->with([
+            'message' => "An error occurred",
+            "success" => false
+        ]);
     }
 }
