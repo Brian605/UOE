cc7c57294f7d81434b7088facbac90bfb577a0a2
kemboi22 <kemboielvis22@gmail.com>
2024-07-23 13:35:41 +0300

    feat(farm-plans): global confirm delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+const confirmDelete = (form, id) => {
+  if (confirm('Are you sure you want to delete this record?') ===true) {
+    document.getElementById(`${form}${id}`).submit();
+  }
+}
+window.confirmDelete = confirmDelete;
