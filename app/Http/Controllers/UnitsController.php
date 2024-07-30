c9d6421f79b0cd7daf7a82321b553a12de23b07e
kemboi22 <kemboielvis22@gmail.com>
2024-07-30 04:48:19 +0300

    feat(units): ability to add unit, edit and delete units

---
+++ app/Http/Controllers/UnitsController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Units;
+use Illuminate\Http\Request;
+
+class UnitsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('units.index', [
+            'units' => Units::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $units = Units::query()->create([
+            'name' => $request->name,
+            'unit' => $request->unit,
+        ]);
+        if ($units)
+        {
+            return to_route('units.index')->with([
+                "message" => "Units successfully created",
+                "success" => true
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Units $units)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Units $units)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $units = Units::query()->findOrFail($id);
+        if ($units)
+        {
+            $updated = $units->update([
+                'name' => $request->name,
+                'unit' => $request->unit,
+            ]);
+            if ($updated)
+            {
+                return to_route('units.index')->with([
+                    "message" => "Updated Successfully",
+                    "success" => true
+                ]);
+            }
+            return back() ->with([
+                "message" => "An error occurred",
+                "success" => false
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $units = Units::query()->findOrFail($id);
+        if ($units)
+        {
+            if ($units->delete())
+            {
+                return to_route('units.index')->with([
+                    "message" => "Deleted Successfully",
+                    "success" => true
+                ]);
+            }
+            return back()->with([
+                "message" => "An error occurred",
+                "success" => false
+            ]);
+        }
+        return back()->with([
+            "message" => "An error occurred",
+            "success" => false
+        ]);
+    }
+}
---
+++ resources/views/units/index.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="block block-rounded">
+        <div class="block-header block-header-default">
+            <h3 class="block-title">Crops</h3>
+        </div>
+        <div class="block-content block-content-full">
+            @can('units.index')
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createUnits">
+                    Create Units
+                </button>
+            @endcan
+
+            <!-- DataTables init on table by adding .js-dataTable-buttons class, functionality is initialized in js/pages/tables_datatables.js -->
+            <table class="table table-bordered table-striped table-vcenter js-dataTable-buttons">
+                <thead>
+                <tr>
+                    <th class="text-center" style="width: 80px;">#</th>
+                    <th>Name</th>
+                    <th>Abbreviation Unit</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($units as $index => $unit)
+                    <tr>
+                        <td class="text-center">{{ $index+1 }}</td>
+                        <td class="fw-semibold">
+                            {{ $unit->name }}
+                        </td>
+                        <td>
+                            {{ $unit->unit }}
+                        </td>
+                        <td class="d-flex gap-4">
+                            @can('units.edit')
+                                <button class="btn btn-secondary" data-bs-toggle="modal"
+                                        data-bs-target="#editCrop" onclick="editForm({{ $unit }})">Edit
+                                </button>
+                            @endcan
+                            @can('units.destroy')
+                                <div>
+                                    <form id="deleteForm{{$unit->id}}"
+                                          action="{{ route('units.destroy', $unit->id) }}"
+                                          method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" class="btn btn-danger" onclick="confirmDelete('deleteForm',
+                                    {{ $unit->id }})">Delete
+                                        </button>
+                                    </form>
+                                </div>
+                            @endcan
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
+        <!-- Modal -->
+        <div class="modal fade" id="createUnits" tabindex="-1" aria-labelledby="createUnitsLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="createUnitsLabel">Add Unit</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="{{ route('units.store') }}" method="post">
+                        <div class="modal-body">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="name" name="name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="unit" class="form-label">Abbreviation Unit</label>
+                                <input type="text" class="form-control" id="unit" name="unit" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="editUnit" tabindex="-1" aria-labelledby="editUnitLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="editUnitLabel">Edit Unit</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form id="editForm" method="post">
+                        <div class="modal-body">
+                            @csrf
+                            @method('PUT')
+                            <div class="mb-3">
+                                <label for="edit_name" class="form-label">Name</label>
+                                <input type="text" class="form-control" id="edit_name" name="name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="edit_unit" class="form-label">Abbreviation Unit</label>
+                                <input type="text" class="form-control" id="edit_unit" name="unit" required>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script>
+        const editForm = (unit) => {
+            const form = document.getElementById('editForm')
+            form.action = `/units/${unit.id}`
+            document.getElementById('edit_name').value = unit.name
+            document.getElementById('edit_unit').value = unit.unit
+        }
+    </script>
+@endsection
